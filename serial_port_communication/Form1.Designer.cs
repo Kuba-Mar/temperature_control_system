276a50ed5a2d3e7dd1b7e33a053beb6547e942dc
Maksymilian Pływaczyk <plywak12331@gmail.com>
2022-01-25 23:48:00 +0100

    Demo version of .net app

--- serial_port_communication/Form1.Designer.cs
+++ serial_port_communication/Form1.Designer.cs
@@ -30,12 +30,231 @@ namespace serial_port_communication
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Refresh_button = new System.Windows.Forms.Button();
+            this.COM_comboBox = new System.Windows.Forms.ComboBox();
+            this.Baud_Rate_comboBox = new System.Windows.Forms.ComboBox();
+            this.Data_Bits_comboBox = new System.Windows.Forms.ComboBox();
+            this.Stop_bits_comboBox = new System.Windows.Forms.ComboBox();
+            this.Parity_comboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Send_button = new System.Windows.Forms.Button();
+            this.SendTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Read_button = new System.Windows.Forms.Button();
+            this.ReceivedTextBox = new System.Windows.Forms.TextBox();
+            this.Clear_button = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Refresh_button
+            // 
+            resources.ApplyResources(this.Refresh_button, "Refresh_button");
+            this.Refresh_button.Name = "Refresh_button";
+            this.Refresh_button.UseVisualStyleBackColor = true;
+            this.Refresh_button.Click += new System.EventHandler(this.Refresh_button_Click);
+            // 
+            // COM_comboBox
+            // 
+            this.COM_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.COM_comboBox, "COM_comboBox");
+            this.COM_comboBox.FormattingEnabled = true;
+            this.COM_comboBox.Name = "COM_comboBox";
+            // 
+            // Baud_Rate_comboBox
+            // 
+            this.Baud_Rate_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.Baud_Rate_comboBox, "Baud_Rate_comboBox");
+            this.Baud_Rate_comboBox.FormattingEnabled = true;
+            this.Baud_Rate_comboBox.Items.AddRange(new object[] {
+            resources.GetString("Baud_Rate_comboBox.Items"),
+            resources.GetString("Baud_Rate_comboBox.Items1"),
+            resources.GetString("Baud_Rate_comboBox.Items2"),
+            resources.GetString("Baud_Rate_comboBox.Items3"),
+            resources.GetString("Baud_Rate_comboBox.Items4"),
+            resources.GetString("Baud_Rate_comboBox.Items5"),
+            resources.GetString("Baud_Rate_comboBox.Items6"),
+            resources.GetString("Baud_Rate_comboBox.Items7")});
+            this.Baud_Rate_comboBox.Name = "Baud_Rate_comboBox";
+            // 
+            // Data_Bits_comboBox
+            // 
+            this.Data_Bits_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.Data_Bits_comboBox, "Data_Bits_comboBox");
+            this.Data_Bits_comboBox.FormattingEnabled = true;
+            this.Data_Bits_comboBox.Items.AddRange(new object[] {
+            resources.GetString("Data_Bits_comboBox.Items"),
+            resources.GetString("Data_Bits_comboBox.Items1"),
+            resources.GetString("Data_Bits_comboBox.Items2"),
+            resources.GetString("Data_Bits_comboBox.Items3")});
+            this.Data_Bits_comboBox.Name = "Data_Bits_comboBox";
+            // 
+            // Stop_bits_comboBox
+            // 
+            this.Stop_bits_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.Stop_bits_comboBox, "Stop_bits_comboBox");
+            this.Stop_bits_comboBox.FormattingEnabled = true;
+            this.Stop_bits_comboBox.Items.AddRange(new object[] {
+            resources.GetString("Stop_bits_comboBox.Items"),
+            resources.GetString("Stop_bits_comboBox.Items1"),
+            resources.GetString("Stop_bits_comboBox.Items2")});
+            this.Stop_bits_comboBox.Name = "Stop_bits_comboBox";
+            // 
+            // Parity_comboBox
+            // 
+            this.Parity_comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.Parity_comboBox, "Parity_comboBox");
+            this.Parity_comboBox.FormattingEnabled = true;
+            this.Parity_comboBox.Items.AddRange(new object[] {
+            resources.GetString("Parity_comboBox.Items"),
+            resources.GetString("Parity_comboBox.Items1"),
+            resources.GetString("Parity_comboBox.Items2"),
+            resources.GetString("Parity_comboBox.Items3"),
+            resources.GetString("Parity_comboBox.Items4"),
+            resources.GetString("Parity_comboBox.Items5")});
+            this.Parity_comboBox.Name = "Parity_comboBox";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.BackColor = System.Drawing.Color.Green;
+            resources.ApplyResources(this.Connect_button, "Connect_button");
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.UseVisualStyleBackColor = false;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_button_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Send_button);
+            this.groupBox1.Controls.Add(this.SendTextBox);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // Send_button
+            // 
+            resources.ApplyResources(this.Send_button, "Send_button");
+            this.Send_button.Name = "Send_button";
+            this.Send_button.UseVisualStyleBackColor = true;
+            this.Send_button.Click += new System.EventHandler(this.Send_button_Click);
+            // 
+            // SendTextBox
+            // 
+            resources.ApplyResources(this.SendTextBox, "SendTextBox");
+            this.SendTextBox.Name = "SendTextBox";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Read_button);
+            this.groupBox2.Controls.Add(this.ReceivedTextBox);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // Read_button
+            // 
+            resources.ApplyResources(this.Read_button, "Read_button");
+            this.Read_button.Name = "Read_button";
+            this.Read_button.UseVisualStyleBackColor = true;
+            this.Read_button.Click += new System.EventHandler(this.Read_button_Click);
+            // 
+            // ReceivedTextBox
+            // 
+            resources.ApplyResources(this.ReceivedTextBox, "ReceivedTextBox");
+            this.ReceivedTextBox.Name = "ReceivedTextBox";
+            this.ReceivedTextBox.ReadOnly = true;
+            // 
+            // Clear_button
+            // 
+            resources.ApplyResources(this.Clear_button, "Clear_button");
+            this.Clear_button.Name = "Clear_button";
+            this.Clear_button.UseVisualStyleBackColor = true;
+            this.Clear_button.Click += new System.EventHandler(this.Clear_button_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.Clear_button);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Connect_button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Parity_comboBox);
+            this.Controls.Add(this.Stop_bits_comboBox);
+            this.Controls.Add(this.Data_Bits_comboBox);
+            this.Controls.Add(this.Baud_Rate_comboBox);
+            this.Controls.Add(this.COM_comboBox);
+            this.Controls.Add(this.Refresh_button);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Refresh_button;
+        private System.Windows.Forms.ComboBox COM_comboBox;
+        private System.Windows.Forms.ComboBox Baud_Rate_comboBox;
+        private System.Windows.Forms.ComboBox Data_Bits_comboBox;
+        private System.Windows.Forms.ComboBox Stop_bits_comboBox;
+        private System.Windows.Forms.ComboBox Parity_comboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Send_button;
+        private System.Windows.Forms.TextBox SendTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Read_button;
+        private System.Windows.Forms.TextBox ReceivedTextBox;
+        private System.Windows.Forms.Button Clear_button;
+        private System.IO.Ports.SerialPort serialPort1;
     }
 }
 
--- serial_port_communication/Form1.cs
+++ serial_port_communication/Form1.cs
@@ -7,14 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace serial_port_communication
 {
     public partial class Form1 : Form
     {
+        System.IO.Ports.SerialPort serialPort;
+        
+   
         public Form1()
         {
             InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.ReadTimeout = 500;
+            serialPort.WriteTimeout = 500;
+
         }
+
+
+        private void Connect_button_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                MessageBox.Show("Already connected to the device");
+            }
+            else
+                try
+                {
+                    serialPort.PortName = this.COM_comboBox.Text;
+                    serialPort.BaudRate = Int32.Parse(this.Baud_Rate_comboBox.Text);
+                    serialPort.DataBits = Int32.Parse(this.Data_Bits_comboBox.Text);
+                    serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), this.Stop_bits_comboBox.Text);
+                    serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), this.Parity_comboBox.Text);
+                    serialPort.Open();
+                }
+                catch (Exception exc)
+                {
+                    MessageBox.Show("Connection error: " + exc.Message);
+                }
+        }
+        private void Read_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ReceivedTextBox.Text = serialPort.ReadLine();
+            }
+            catch (TimeoutException)
+            {
+                ReceivedTextBox.Text = "Timeout Exception";
+            }
+        }
+        private void Send_button_Click(object sender, EventArgs e)
+        {
+            serialPort.WriteLine(SendTextBox.Text);
+            SendTextBox.Text = "";
+        }
+        private void Refresh_button_Click(object sender, EventArgs e)
+        {
+            Refresh_button.Text = Char.ConvertFromUtf32(81);
+            updateAvailablePorts();
+        }
+
+        private void updateAvailablePorts()
+        {
+            COM_comboBox.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                COM_comboBox.Items.Add(s);
+            }
+        }
+
+        private void Clear_button_Click(object sender, EventArgs e)
+        {
+            SendTextBox.Clear();
+            ReceivedTextBox.Clear();
+        }
+
+      
     }
 }
